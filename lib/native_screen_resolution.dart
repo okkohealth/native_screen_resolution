7b50c23c0d4529e98cccfc85d9130c05f6f130af
Darren Perry <darren@okkohealth.com>
2021-03-25 16:37:02 +0000

    - int/double parsing

--- lib/native_screen_resolution.dart
+++ lib/native_screen_resolution.dart
@@ -10,7 +10,9 @@ class NativeScreenResolution {
   static Future<Size> get screenResolution async {
     final String version = await _channel.invokeMethod('getScreenResolution');
     Map<String, dynamic> map = jsonDecode(version);
-    Size size = Size(map["width"], map["height"]);
+    double width = (map["width"] is int) ? (map["width"] as int).toDouble() : map["width"];
+    double height = (map["height"] is int) ? (map["height"] as int).toDouble() : map["height"];
+    Size size = Size(width, height);
     return size;
   }
 }
